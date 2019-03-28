262866c3caedddf53205e7de964c1b13442fc486
Daniel Paulson <daniel.paulson20@gmail.com>
2019-03-27 22:57:18 -0700

    fixed what I messed up with Cameron's code

--- Access4All/Fragments/detaildepthFragment.cs
+++ Access4All/Fragments/detaildepthFragment.cs
@@ -158,6 +158,67 @@ namespace Access4All.Fragments
             return v;   
         }
 
+        private string parseMainEnterance(string unparsedData, string curLocation)
+        {
+            JArray jsonArray = JArray.Parse(unparsedData);
+            string data = "";
+
+            int main_ent_id;
+            int total_num_public_entrances;
+            string main_ent_accessible;
+            string alt_ent_accessible;
+            string accessable_signage;
+            string ground_level;
+            string threshold_level;
+            string threshold_beveled;
+            string beveled_height;
+            string door_action;
+            string door_open_clearance;
+            double door_open_force;
+            string door_use_with_fist;
+            string door_auto_open;
+            string second_door_inside;
+            string min_dist_between_doors;
+            string lighting;
+            string lighting_option;
+            string comment;
+            string recommendations; //currently all items are empty
+
+
+            for (int i = 0; i < jsonArray.Count; i++)//this should only ever be one, but keep it here in case something goes wrong?
+            {
+                JToken json = jsonArray[i];
+
+                if (((int)json["est_id"]) == est_id)
+                {
+                    main_ent_id = (int)json["main_ent_id"];
+                    total_num_public_entrances = ((int)json["total_num_public_entrances"]);
+                    main_ent_accessible = ((string)json["main_ent_accessible"]).ToLower();
+                    alt_ent_accessible = ((string)json["alt_ent_accessible"]).ToLower();
+                    accessable_signage = ((string)json["accessable_signage"]).ToLower();
+                    ground_level = ((string)json["ground_level"]).ToLower();
+                    threshold_level = ((string)json["threshold_level"]).ToLower();
+                    threshold_beveled = ((string)json["threshold_beveled"]).ToLower();
+                    beveled_height = ((string)json["beveled_height"]).ToLower();
+                    door_open_clearance = ((string)json["door_open_clearance"]).ToLower();
+                    door_action = ((string)json["door_action"]).ToLower();
+                    door_open_force = ((double)json["door_open_force"]);
+                    door_use_with_fist = ((string)json["door_use_with_fist"]).ToLower();
+                    door_auto_open = ((string)json["door_auto_open"]).ToLower();
+                    second_door_inside = ((string)json["second_door_inside"]).ToLower();
+                    min_dist_between_doors = ((string)json["min_dist_between_doors"]).ToLower();
+                    lighting = ((string)json["lighting"]).ToLower();
+                    lighting_option = ((string)json["lighting_option"]).ToLower();
+                    comment = ((string)json["comment"]).ToLower();
+                    recommendations = ((string)json["recommendations"]).ToLower();
+
+                    data += ("• " + "The establishment has " + total_num_public_entrances + " public entrances.\n\r");
+                }
+            }
+
+            return data;
+        }
+
         private string parseExteriorData(string unparsedData)
         {
             JArray jsonArray = JArray.Parse(unparsedData);
